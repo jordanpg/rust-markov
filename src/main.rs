b6ada10112c716c0c0b0d1bad5400995aaec8cc7
Jordan Gilbreath <jpg0016@uah.edu>
2022-04-10 01:03:46 -0500

    implement basic function

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,21 @@
+use crate::markov::MarkovBot;
+
 pub mod markov;
 
 fn main() {
-    let a = "testing this thing";
-    let a = a.split(' ');
-    for s in a {
-        println!("{}", s);
+    let mut bot = MarkovBot::new();
+    bot.add_link("", "test1");
+    bot.add_link("", "test2");
+
+    let mut a = 0;
+    let mut b = 0;
+    for _ in 0..10000 {
+        match bot.chain("") {
+            "test1" => a += 1,
+            "test2" => b += 1,
+            _ => panic!("unknown answer"),
+        }
     }
+
+    println!("{} {}", a, b);
 }
--- src/markov.rs
+++ src/markov.rs
@@ -1,58 +1,56 @@
-pub mod markov {
-	pub struct Link<'a> {
-		pub value: u32,
-		node: &'a Node<'a>
-	}
-	
-	pub struct Node<'a> {
-		pub value: &'a str,
-		links: Vec<Link<'a>>
-	}
-	
-	pub trait MarkovNode {
-		
+use petgraph::{graphmap::DiGraphMap};
+use indexmap::set::IndexSet;
+use rand::{Rng, prelude::ThreadRng};
+
+pub struct MarkovBot<'a> {
+	graph: DiGraphMap<usize, u32>,
+	words: IndexSet<&'a str>,
+	rng: ThreadRng
+}
+
+impl<'a> MarkovBot<'a> {
+	pub fn new() -> Self {
+		Self::default()
 	}
 
-	impl Link<'_> {
-		pub fn strengthen(&mut self) -> u32 {
-			self.value += 1;
+	pub fn add_link(&mut self, a: &'a str, b: &'a str) -> u32 {
+		let (id_a, _) = self.words.insert_full(a);
+		let (id_b, _) = self.words.insert_full(b);
 
-			self.value
-		}
+		let weight = self.graph.edge_weight(id_a, id_b).unwrap_or(&0) + 1;
+		self.graph.add_edge(id_a, id_b, weight);
+
+		weight
 	}
 
-	impl<'a> Node<'a> {
-		fn search_links(&mut self, search: &str) -> Option<&'a mut Link> {
-			for i in self.links.iter_mut() {
-				if i.node.value.eq_ignore_ascii_case(search) {
-					return Some(i)
+	pub fn chain(&mut self, word: &str) -> &str {
+		match self.words.get_index_of(word) {
+			Some(id) => {
+				let edges = self.graph.edges(id).collect::<Vec<_>>();
+				let mut t = 0;
+				let mut w = Vec::<u32>::with_capacity(edges.len());
+				for (_, _, wt) in &edges {
+					t += **wt;
+					w.push(t);
 				}
-			}
 
-			None
-		}
-
-		fn find_link(&mut self, n: &'a Node<'a>) -> Option<&'a mut Link> {
-			for i in self.links.iter_mut() {
-				if std::ptr::eq(i.node, n) {
-					return Some(i)
+				let s = self.rng.gen_range(0..(t));
+				// println!("{} {}", t, s);
+				let ind = w.into_iter().position(|wt| wt > s).unwrap_or(0);
+				match edges.get(ind) {
+					Some((_, b, _)) => *self.words.get_index(*b).unwrap_or(&""),
+					None => "",
 				}
-			}
-
-			None
-		}
-
-		pub fn add_new_link(&mut self, n: &'a Node<'a>) {
-			self.links.push(Link { value: 1, node: n });
-		}
-
-		pub fn strengthen_or_link(&'a mut self, n: &'a Node<'a>) {
-			match self.find_link(n) {
-				Some(l) => {
-					l.strengthen();
-				}
-				None => {}
-			}
+			},
+			None => return "",
 		}
 	}
+}
+
+impl Default for MarkovBot<'_> {
+    fn default() -> Self {
+        let mut s = Self { graph: Default::default(), words: IndexSet::with_capacity(1), rng: rand::thread_rng() };
+		s.words.insert("");
+		s
+    }
 }
